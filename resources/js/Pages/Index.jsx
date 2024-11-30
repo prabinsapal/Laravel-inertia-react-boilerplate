10fd9d86bce4af3a70a2cd4d6d111e67198ed709
Prabin Sapal <coderprabin@gmail.com>
2024-11-30 12:19:30 +0545

    Initial commit with laravel react boilerplate

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+    return (
+        <>
+            <h1 style={{textAlign:"center"}}>Welcome to Laravel 11 with inertia js and react 18.</h1>
+        </>
+    )
+}
